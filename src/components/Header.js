d7f96b060081bb161ce98ff0b9bc2c610d578664
Jamie <jmeleacc@gmail.com>
2021-08-08 16:52:28 -0500

    Finish tabs

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Tab from './Tab'
+import {NavLink} from 'react-router-dom'
+
+
+export default function Header() {
+return (
+<div className="tabs">
+
+  <Tab><NavLink to="/">Home</NavLink>
+  </Tab>
+
+  <Tab><NavLink to="/about">About</NavLink>
+  </Tab>
+
+  <Tab><NavLink to="/features">Features</NavLink>
+  </Tab>
+</div>
+)
+}
